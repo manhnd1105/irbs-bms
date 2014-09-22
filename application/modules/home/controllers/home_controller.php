6acecb238375a4e69301ce859e56df40ec48540f
manhnd1105 <manhnd.work@gmail.com>
2014-09-22 15:30:43 +0700

    init

--- application/modules/home/controllers/home_controller.php
+++ application/modules/home/controllers/home_controller.php
@@ -25,24 +25,6 @@ class Home_controller extends Backend_Controller
     public function index()
     {
         //Call the layout (as method of template controller) that you want to display
-        //$data['actions'] = $this->init_actions();
         $this->template_controller->demo_template('home', 'index');
     }
-
-    /**
-     * @return array
-     */
-    function init_actions()
-    {
-        $actions = array(
-            anchor('account/account_controller', 'Manage Inkiu accounts'),
-            anchor('account/account_controller/view_create', 'Create Inkiu account'),
-            anchor('authentication/authentication_controller/', 'Authentication'),
-            anchor('rbac/perm_controller/', 'Manage permissions'),
-            anchor('rbac/role_controller/', 'Manage roles'),
-            anchor('http://inkiu.vn/irbs-dms/index.php/api/api_controller/order', 'Access to external system: get list orders')
-        );
-        return $actions;
-    }
-
 }
