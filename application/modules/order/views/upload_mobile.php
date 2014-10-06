fc2e6af69f900da9c20efad54bc1368cbed8218d
DinhTuanLong <dinhtuanlong1329@gmail.com>
2014-10-07 00:41:17 +0700

    10.2. Thiết kế form upload dạng classic cho phiên bản mobile

--- application/modules/order/views/upload_mobile.php
+++ application/modules/order/views/upload_mobile.php
@@ -5,16 +5,52 @@
  * Date: 10/6/2014
  * Time: 3:17 PM
  */
+echo '<link rel="stylesheet" href="'.base_url() . 'assets/css/admin_template/upload.css"/>';
+echo
+'<div class="row">
+    <div class="panel panel-default">';
+
 /** @var $module string */
 /** @var $controller string */
 /** @var $action string */
-
 echo form_open($module . '/' . $controller . '/' . $action,'enctype="multipart/form-data" id="uploadForm"');
-echo form_fieldset('Mobile form');
-echo '<input id="upload" type="file" name="upload"/>';
-echo '<button class="btn btn-danger btn-sm" type="submit" id="submit" name="submit">
-<i class="fa fa-upload fa-fw"></i>Upload
-</button>';
+//echo '<input id="upload" type="file" name="upload"/>';
+echo '<div class="panel-heading">
+        <div class="btn-group-xs">';
+echo '<a class="btn btn-warning fileinput-button">
+         <i class="fa fa-file-image-o fa-fw"></i>
+         <span>Add images...</span>';
+echo '<input type="file" name="files[]" multiple="multiple">';
+echo '</a>';
+echo '<button type="submit" class="btn btn-primary">
+                    <i class="fa fa-cloud-upload fa-fw"></i>
+                    <span>Start upload</span>
+                </button>';
+echo'<button type="reset" class="btn btn-outline btn-default pull-right">
+                    <i class="fa fa-refresh fa-fw"></i>
+                </button>
+            </div>
+        </div>';
+//end panel-heading & form
+echo '<div class="panel-body" id="images_upload">
+            <div class="row">
+                <div class="col-xs-4">
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/200x200" alt="..."/>
+                    </a>
+                </div>
+                <div class="col-xs-4">
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/200x200" alt="..."/>
+                    </a>
+                </div>
+                <div class="col-xs-4">
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/200x200" alt="..."/>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>';
 
-//echo form_submit('submit','<i class="fa fa-upload fa-fw"></i>Upload','class="btn btn-danger btn-sm" id="submit"');
-//echo form_button('submit', '<i class="fa fa-upload fa-fw"></i>Upload', 'class="btn btn-danger btn-sm" id="submit"');
