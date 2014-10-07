4b800adf3266f1853945b58754283d97ef511b3f
DinhTuanLong <dinhtuanlong1329@gmail.com>
2014-10-07 12:55:02 +0700

    10.1 Thiet ke upload web full

--- application/modules/order/views/js/upload.js
+++ application/modules/order/views/js/upload.js
@@ -3,30 +3,88 @@
  */
 
 $(document).ready(function(){
-    $("#uploadForm").on('submit',(function(e) {
-        e.preventDefault();
-        var  data = new FormData(this);
-        //var images = $('#upload').files;
-        //for(var i = 0; i <images.length; i++){
-        //    data.append('upload',images.file[i]);
-        //}
-        $.ajax({
-            'url': "http://localhost/php_ajax_image_upload/upload.php",
-            'data': '{"0"=>"ss","1"==>"dsdasds"}',
-            'contentType': false,
-            'cache': false,
-            'processData':false,
-            'success': function(data)
-            {
-                $("#uploaded").html(data);
-            },
-            'error': function()
-            {
-                $("#uploaded").html("error ===========");
+    //$("#files").on('submit',(function(e) {
+    //    e.preventDefault();
+    //    var  data = new FormData(this);
+    //    //var images = $('#upload').files;
+    //    //for(var i = 0; i <images.length; i++){
+    //    //    data.append('upload',images.file[i]);
+    //    //}
+    //    $.ajax({
+    //        'url': "http://localhost/php_ajax_image_upload/upload.php",
+    //        'data': data,
+    //        'contentType': false,
+    //        'cache': false,
+    //        'processData':false,
+    //        'success': function(data)
+    //        {
+    //            $("#uploaded").html(data);
+    //        },
+    //        'error': function()
+    //        {
+    //            $("#uploaded").html("error ===========");
+    //        }
+    //    });
+    //
+    //}));
+    var formdata = false;
+
+
+    if(window.FormData){
+        formdata = new FormData();
+    }
+
+    function showUploadItem(source, file){
+        var tbody = document.getElementById('items_upload');
+        //var th = document.createElement('thead');
+        //var th_row = document.createElement('row');
+        //
+        //
+        //var header = table.createTHead();
+        //var theader = header.insertRow();
+        //var item_no = theader.insertCell(0);
+        //item_no.innerHTML = '<p class="text-center">#</p>';
+        //var item_src = theader.insertCell(1);
+        //item_src.innerHTML = '<p class="text-center"><i class="fa fa-image"></i></p>';
+        //var item_name = theader.insertCell(2);
+        //item_name.innerHTML = '<p class="text-center">Name</p>';
+        //var item_type = theader.insertCell(3);
+        //item_type.innerHTML = '<p class="text-center">Type</p>';
+        //var item_size = theader.insertCell(4);
+        //item_size.innerHTML = '<p class="text-center">Size</p>';
+        //var action = theader.insertCell(5);
+        //action.innerHTML = '<p class="text-center">Action</p>';
+
+        var row = tbody.insertRow();
+        row.insertCell(0).innerHTML = 1;
+        row.insertCell(1).innerHTML = '<p class="text-center"><img src="'+source+'" class="img-thumbnail" width = "90px;" height ="90px;"/></p>';
+        row.insertCell(2).innerHTML = '<p class="text-center">'+file.name+'</p>';
+        row.insertCell(3).innerHTML = '<p class="text-center">'+file.type+'</p>';
+        row.insertCell(4).innerHTML = '<p class="text-center">'+file.size+'</p>';
+        row.insertCell(5).innerHTML = '<p class="text-center">'+'action'+'</p>';
+    }
+
+
+    $('#files').on('change',(function(event){
+        event.preventDefault();
+
+        var i = 0, reader, file;
+        for( ; i< this.files.length; i++){
+            file = this.files[i];
+            if(file.type.match(/image.*/)){
+                if(window.FileReader){
+                    reader = new FileReader();
+                    reader.onloadend = function(e){
+                        showUploadItem(e.target.result,file);
+                    };
+                    reader.readAsDataURL(file);
+                }
+                if(formdata){
+                    formdata.append('files[]',file);
+                }
             }
-        });
+        }
 
     }));
-
 });
 
--- application/modules/order/views/upload.php
+++ application/modules/order/views/upload.php
@@ -1,10 +1,8 @@
 
 <?php
 echo "<script src='" . base_url() . "application/modules/order/views/js/upload.js'></script>";
-/** @var $module string */
-/** @var $controller string */
-/** @var $action string */
-//
+
+
 //echo form_open('http://localhost/irbs-fms/index.php/file/image_controller/view_crud','enctype="multipart/form-data" id="uploadForm"');
 //echo form_fieldset('Upload your images');
 //
@@ -17,15 +15,64 @@ echo "<script src='" . base_url() . "application/modules/order/views/js/upload.j
 //              <i class="glyphicon glyphicon-upload"></i> Upload
 //          </button>';
 //    echo ' <div id="upload_progress" class="progress"></div>';
+//echo form_button('submit', '<i class="fa fa-upload fa-fw"></i>Upload', 'class="btn btn-danger btn-sm" id="submit"');
+
+
+//echo form_submit('submit','<i class="fa fa-upload fa-fw"></i>Upload','class="btn btn-danger btn-sm" id="submit"');
+//echo form_button('submit', '<i class="fa fa-upload fa-fw"></i>Upload', 'class="btn btn-danger btn-sm" id="submit"');
+
+
+echo '<link rel="stylesheet" href="'.base_url() . 'assets/css/admin_template/upload.css"/>';
+echo
+'<div class="row">
+    <div class="panel panel-default" style="border: none;">';
+
+/** @var $module string */
+/** @var $controller string */
+/** @var $action string */
+echo form_open($module . '/' . $controller . '/' . $action,'enctype="multipart/form-data" id="uploadForm"');
+//echo '<input id="upload" type="file" name="upload"/>';
+echo '<div class="panel-heading">
+        <div class="btn-group-sm">';
+echo '<a class="btn btn-warning fileinput-button">
+         <i class="fa fa-file-image-o fa-fw"></i>
+         <span>Add images...</span>';
+echo '<input type="file" name="files" multiple="multiple" accept="image/*" id="files">';
+echo '</a>';
+echo '<button type="submit" class="btn btn-primary">
+                    <i class="fa fa-cloud-upload fa-fw"></i>
+                    <span>Start upload</span>
+                </button>';
+echo'<button type="reset" class="btn btn-outline btn-default">
+                    <i class="fa fa-refresh fa-fw"></i>Refresh
+                </button>
+            </div>
+        </div>';
+//end panel-heading & form
+echo '<div class="panel-body">
+        <div class="table-responsive" id="upload_table">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <td class="text-center">#</td>
+                        <td class="text-center"><i class="fa fa-image"></i></td>
+                        <td class="text-center">Name</td>
+                        <td class="text-center">Type</td>
+                        <td class="text-center">Size</td>
+                        <td class="text-center">Action</td>
+                    </tr>
+                </thead>
+                <tbody id="items_upload">
+                </tbody>
+            </table>
+        </div>
+
+      </div>
+
+'.'</div>
+</div>';
+
 ?>
-<form id="uploadForm" enctype="multipart/form-data" method="post">
-    <input id="upload" type="file" name="upload" multiple="multiple"/>
-    <input type="submit" id="submit" class=" btn btn-danger btn-sm" value="Upload">
-</form>
-<div id="uploaded" class="row"></div>
-
-
-
 
 
 
