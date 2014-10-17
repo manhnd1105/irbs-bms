4ad7b9265af68f687abea89415e61e1f6a48babc
fan_kevin <rong9x@gmail.com>
2014-10-17 09:15:40 +0700

    add chuc nang hien thi bang gia

--- application/modules/price_list/views/price_info.php
+++ application/modules/price_list/views/price_info.php
@@ -1,38 +1,75 @@
-<div class="Table">
-    <div class="Heading">
-        <div class="Cell">
-            <p>ID</p>
-        </div>
-        <div class="Cell">
-            <p>Price</p>
-        </div>
-        <div class="Cell">
-            <p>Description</p>
-        </div>
-        <div class="Cell">
-            <p>OrderId</p>
-        </div>
-        <div class="Cell">
-            <p>Quantity</p>
-        </div>
-        <div class="Cell">
-            <p>Total</p>
-        </div>
-    </div>
-
+<!--<div class="Table">-->
+<!--    <div class="Heading">-->
+<!--        <div class="Cell">-->
+<!--            <p>ID</p>-->
+<!--        </div>-->
+<!--        <div class="Cell">-->
+<!--            <p>Price</p>-->
+<!--        </div>-->
+<!--        <div class="Cell">-->
+<!--            <p>Description</p>-->
+<!--        </div>-->
+<!--        <div class="Cell">-->
+<!--            <p>OrderId</p>-->
+<!--        </div>-->
+<!--        <div class="Cell">-->
+<!--            <p>Quantity</p>-->
+<!--        </div>-->
+<!--        <div class="Cell">-->
+<!--            <p>Total</p>-->
+<!--        </div>-->
+<!--    </div>-->
+<!---->
 <?php
-foreach ($price_info as $row) {
-    echo "<div class='Row'>";
-    $price_id_id = $row['id'];
-    foreach ($row as $cell) {
-        if (!is_array($cell)) {
-            echo "<div class='Cell'>";
-            echo $cell;
-            echo "</div>";
-        }
-    }
+//foreach ($price_info as $row) {
+//    echo "<div class='Row'>";
+//    $price_id_id = $row['id'];
+//    foreach ($row as $cell) {
+//        if (!is_array($cell)) {
+//            echo "<div class='Cell'>";
+//            echo $cell;
+//            echo "</div>";
+//        }
+//    }
+//
+//    /* End of a row */
+//    echo '</div>';
+//}
+//
 
-    /* End of a row */
-    echo '</div>';
+echo '<div class="row">';
+
+echo '<div  class="col-sm-12">';
+
+
+echo '<div class="panel panel-default">';
+echo '<div class="panel-heading">Account Management</div>';
+echo '<div class="table-responsive">';
+echo '<table class="table">';
+echo '<tr>';
+echo '<td> ID</td>';
+echo '<td> Price </td>';
+echo '<td> Description </td>';
+echo '<td> Order ID </td>';
+echo '<td> Quantity </td>';
+echo '<td> Total </td>';
+
+echo '</tr>';
+
+
+foreach ($price_info as $row) {
+    $id = $row['id'];
+
+    echo '<tr class="success"">';
+    echo '<td >' . $id . '</td>';
+    echo '<td >' . $row['price'] . '</td>';
+    echo '<td>' . $row['description'] . '</td>';
+    echo '<td>' . $row['order_id'] . '</td>';
+    echo '<td>' . $row['quantity'] . '</td>';
+    echo '<td>' . $row['total'] . '</td>';
+    echo '</tr>';
 }
-    
+
+echo '</table>';
+echo '</div>';
+echo '</div>';
--- application/modules/template/views/demo_template/side_menu.php
+++ application/modules/template/views/demo_template/side_menu.php
@@ -12,6 +12,7 @@ echo '<li>' . anchor('order/order_controller/view_progress', 'Progress','class="
 echo '<li>' . anchor('order/order_controller/view_payment', 'Payment','class="active"') . '</li>';
 echo '<li>' . anchor('order/order_controller/view_crud_custom', 'Order Custom View','class="active"') . '</li>';
 echo '<li>' . anchor('order/order_controller/view_order_tracking', 'Order Tracking','class="active"') . '</li>';
+echo '<li>' . anchor('price_list/price_controller/view_price', 'View Price','class="active"') . '</li>';
 
 echo '<li><a href="#collapseUpload" data-toggle="collapse" data-parent="#accordion"><i class="fa fa-cloud-upload fa-fw"></i> Upload form<i class="pull-right fa fa-plus"></i></a>'
 .'<div id="collapseUpload" class="panel-collapse collapse">'
